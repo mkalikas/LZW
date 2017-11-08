344067411c176bf3d90f7e6b7476c8fe6ec5883d
Maria <17375691+mkalikas@users.noreply.github.com>
2017-11-08 00:19:54 -0500

    formatted with clang-format

--- lzw.hpp
+++ lzw.hpp
@@ -1,28 +1,28 @@
-#pragma once 
+#pragma once
 
-#include <vector>
+#include <iostream>
 #include <map>
 #include <string>
+#include <vector>
 
-struct lzw
-{
+struct lzw {
   std::map<std::string, int> dictionary;
-  std::map<std::string, int>& build_dictionary();
+  std::map<std::string, int> &build_dictionary();
   std::string int_to_binary(int c, int cl);
   int binary_to_int(std::string p);
+  void binaryIODemo(std::vector<char> compressed);
 };
 
 // Compress a string to a list of output symbols.
 // The result will be written to the output iterator
 // starting at "result"; the final iterator is returned.
 template <typename Iterator>
-Iterator compress(const std::string &uncompressed, Iterator result) {
+Iterator compress(const std::vector<char> &uncompressed, Iterator result) {
   // Build the dictionary.binaryIODemo(v);
   int dictSize = 256;
-  std::map<std::string,int> dictionary;
-  for (int i = 0; i < 256; i++)
-    dictionary[std::string(1, i)] = i;
- 
+  std::map<std::string, int> dictionary;
+  for (int i = 0; i < 256; i++) dictionary[std::string(1, i)] = i;
+
   std::string w;
   for (std::string::const_iterator it = uncompressed.begin();
        it != uncompressed.end(); ++it) {
@@ -33,33 +33,30 @@ Iterator compress(const std::string &uncompressed, Iterator result) {
     else {
       *result++ = dictionary[w];
       // Add wc to the dictionary. Assuming the size is 4096!!!
-      if (dictionary.size()<4096)
-         dictionary[wc] = dictSize++;
+      if (dictionary.size() < 4096) dictionary[wc] = dictSize++;
       w = std::string(1, c);
     }
   }
- 
+
   // Output the code for w.
-  if (!w.empty())
-    *result++ = dictionary[w];
+  if (!w.empty()) *result++ = dictionary[w];
   return result;
 }
- 
+
 // Decompress a list of output ks to a string.
 // "begin" and "end" must form a valid range of ints
 template <typename Iterator>
 std::string decompress(Iterator begin, Iterator end) {
   // Build the dictionary.
   int dictSize = 256;
-  std::map<int,std::string> dictionary;
-  for (int i = 0; i < 256; i++)
-    dictionary[i] = std::string(1, i);
- 
+  std::map<int, std::string> dictionary;
+  for (int i = 0; i < 256; i++) dictionary[i] = std::string(1, i);
+
   std::string w(1, *begin++);
   std::string result = w;
-  std::cout << result<<"???:::\n";
+  std::cout << result << "???:::\n";
   std::string entry;
-  for ( ; begin != end; begin++) {
+  for (; begin != end; begin++) {
     int k = *begin;
     if (dictionary.count(k))
       entry = dictionary[k];
@@ -67,13 +64,12 @@ std::string decompress(Iterator begin, Iterator end) {
       entry = w + w[0];
     else
       throw "Bad compressed k";
- 
+
     result += entry;
- 
+
     // Add w+entry[0] to the dictionary.
-    if (dictionary.size()<4096)
-      dictionary[dictSize++] = w + entry[0];
- 
+    if (dictionary.size() < 4096) dictionary[dictSize++] = w + entry[0];
+
     w = entry;
   }
   return result;
--- lzw_main.cpp
+++ lzw_main.cpp
@@ -1,40 +1,36 @@
 #include <iostream>
 #include "lzw.hpp"
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
   try {
     lzw lzw;
     // Run program passing c and filename to compress
-    switch(*argv[1]) {
+    switch (*argv[1]) {
       // Compress input file
-      case 'c':
-        // Save v file as filename.lzw
-        std::vector<char> v;
-        if (FILE *fp = fopen("filename", "r"))
-        {
-          char buf[1024];
-          while (size_t len = fread(buf, 1, sizeof(buf), fp))
-            v.insert(v.end(), buf, buf + len);
-          fclose(fp);
-        }
-        lzw.compress(v, v.end());
-        copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", "));
-        binaryIODemo(v);
-      // Expand input file
-      case 'e':
+    case 'c': {
+      // Save v file as filename.lzw
+      std::vector<char> v;
+      if (FILE *fp = fopen("filename", "r")) {
+        char buf[1024];
+        while (size_t len = fread(buf, 1, sizeof(buf), fp))
+          v.insert(v.end(), buf, buf + len);
+        fclose(fp);
+      }
+      compress(v, v.end());
+      copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", "));
+      lzw.binaryIODemo(v);
+      }
+        // Expand input file
+      case 'e': {
         // Save expanded file as filename2
         // filename2 should be identical to filename
-        std::string dev = lzw.decompress(v.begin(), v.end());
+        std::string dev = decompress(v.begin(), v.end());
         std::cout << "\n" << dev << "\n";
-
+      }
     }
-    
-  }
-  catch(char const* err) {
-      std::cout << "The library threw an exception:\n"
-        << err << std::endl;
-  }
+    catch (char const *err) {
+      std::cout << "The library threw an exception:\n" << err << std::endl;
+    }
 
-  return 0;
-}
+    return 0;
+  }
